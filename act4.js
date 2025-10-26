86378907db2e5f2b60e73713c9295df100e72d00
adri <adrian.g.job@gmail.com>
2025-10-26 18:50:15 +0100

    fixed 3, finished 4

--- act4.js
+++ act4.js
@@ -1,30 +1,38 @@
 class Esdeveniment { 
+    #stockTickets;
     constructor(name,duration,stockTickets) { 
         this.name = name; 
         this.duration = duration; 
-        this.stockTickets = stockTickets;
+        this.#stockTickets = stockTickets;
+    }
+    #getStockTickets(){
+        console.log(this.#stockTickets);
+        return this.#stockTickets;
     }
     showInfo(){
         Object.keys(this).forEach(attr => {
             console.log(this[attr]);
         });
+        this.#getStockTickets();
     }
     buyTicket(requestedTickets){
+        
         if(requestedTickets > 5 ){
             console.log("cant buy more than 5 tickets");
         }else { 
-            if (this.stockTickets > requestedTickets ) {
+            if (this.#stockTickets > requestedTickets ) {
+                this.#stockTickets -= requestedTickets;
                 console.log("success ");
             }else{ 
                 console.log("there is not enought stock for requested tickets");
             }
         }
-        fury.showInfo();
+        this.showInfo();
     }
     cancelTicket(requestedTickets){ 
-        this.stockTickets = this.stockTickets + requestedTickets;
-        console.log("returned" + requestedTickets + "with success");
-        fury.showInfo();
+        this.#stockTickets +=  requestedTickets;
+        console.log("returned " + requestedTickets + " with success");
+        this.showInfo();
     }
 }
 
@@ -32,4 +40,7 @@ const fury = new Esdeveniment("fury", 12, 50);
 
 fury.showInfo();
 fury.buyTicket(3); 
-fury.buyTicket(6);
+fury.stockTickets = 100;
+fury.cancelTicket(1); 
+
+// cuando muestra 100 es por que ha creado una propiedad y la asigna a fury con el mismo nombre.

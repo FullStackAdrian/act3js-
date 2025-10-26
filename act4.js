4a0e590299418f9f80a9cf61611f5cabce3e007d
adri <adrian.g.job@gmail.com>
2025-10-26 18:59:38 +0100

    added notes to 4, created 5

--- act4.js
+++ act4.js
@@ -44,3 +44,8 @@ fury.stockTickets = 100;
 fury.cancelTicket(1); 
 
 // cuando muestra 100 es por que ha creado una propiedad y la asigna a fury con el mismo nombre.
+// para que nose agregue la propiedad cuando haces fury.stockTickets sin referenciar a la propiedad privada
+// por que si no revienta. Puedes hacer Object.seal(this) dentro del constructor. 
+// esto es por que sella el objeto de manera que no permite añadir ni eliminar. 
+// del contrario a frezze permite modificar y enumerar propiedades existentes, obviamente publicas. 
+// frezze no permite estas y ademas tampoco permite modificar configuraciones, ni valores de propiedades. 

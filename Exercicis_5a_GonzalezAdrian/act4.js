3d2ed21339db38fd337268e4b068bf6f476ee3c5
adri <adrian.g.job@gmail.com>
2025-10-28 22:37:56 +0100

    finished act4

--- Exercicis_5a_GonzalezAdrian/act4.js
+++ Exercicis_5a_GonzalezAdrian/act4.js
@@ -1,20 +1,17 @@
 // Implementa la funció `suma(fn, ...rest)` que sumi tots
 //  els nombres rebuts i retorni el resultat a la `fn`
 //  passada com a callback.
+const resultat = (...rest) => {
+    let resultat = 0;
+    rest.forEach((n) => {
+        resultat += n;
+    });
+    return resultat;
+};
+
 function suma(fn, ...rest) {
-    fn = () =>  {
-        const resultat = 0;
-        rest.forEach(n => {
-            resultat += n;
-        });
-        return resultat;
-    }
-    console.log(fn());
+    return fn(...rest);
 }
 
-// suma(resultat => {
-//     console.log(resultat);
-// 
-// }, 1, 2, 3, 4, 5)
-suma();
+console.log("La resposta es: " + suma(resultat , 1, 2, 3, 4, 5, 6));
 // 15

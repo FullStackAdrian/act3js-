22d32af883a4e95b98ca8fed0a3fbac6b48326ad
adri <adrian.g.job@gmail.com>
2025-10-29 15:47:07 +0100

    in way to resolve xD

--- Exercicis_4a_GonzalezAdrian/act6.js
+++ Exercicis_4a_GonzalezAdrian/act6.js
@@ -1,17 +1,25 @@
-const myArray=[
+const myArray = [
     "Hola",
     12,
     true,
     "Món",
     {},
-    {id:15},
+    { id: 15 },
     ['lala'],
 ]
 
 // nombres, strings, objectes,boolean
 // Fes la funció amb reduce
-function divideixPerTipus(arr){
-
+function divideixPerTipus(arr) {
+   
+    arr.reduce((res, el) => {
+        const typeOf = typeof el;
+        if (!typeOf in res) {
+            res.push(typeOf);
+        }
+        console.log(res + " " + typeOf ); 
+    })
+    return res;
 }
 
 let arr = divideixPerTipus(myArray);

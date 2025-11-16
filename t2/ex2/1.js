e66a0f633bb0da0c0270dbf6726eb25201ee85ad
adri <adrian.g.job@gmail.com>
2025-11-16 23:44:49 +0100

    added getFirstChildrenByType

--- t2/ex2/1.js
+++ t2/ex2/1.js
@@ -1,39 +1,50 @@
 const testDiv = document.getElementById("test");
-const imgsPaths = [
-    "img/gnu.jpg",
-    "img/gnulinux.jpg",
-    "img/linux.jpg"
-]; 
+const imgsPaths = ["img/gnu.jpg", "img/gnulinux.jpg", "img/linux.jpg"];
+
+// devuelve el primer hijo que coincida con el tipo
+function getFirstChildByType(element, type) {
+    for (const child of element.children) {
+        if (child.tagName.toLowerCase() === type.toLowerCase()) {
+            return child;
+        }
+    }
+}
 
 function random(number) {
-  return Math.floor(Math.random() * number);
+    return Math.floor(Math.random() * number);
 }
 
 function createRandomImg() {
-    const newImg = new Image(125,125);
+    const newImg = new Image(125, 125);
     newImg.src = imgsPaths[random(3)];
     return newImg;
 }
 
-function loadImages() {
-    const images = document.querySelectorAll("img");
-    images.forEach(img => {
+// carga las imagenes aleatorias
+function loadImages(images) {
+    images.forEach((img) => {
         img.replaceWith(createRandomImg());
     });
 }
 
-function loadH2Styles(){
-    const h2elements = document.querySelectorAll("h2");
-    h2elements.forEach(h2 => {
-         
+function loadH2Styles(h2s) {
+    h2s.forEach((h2) => {
+        h2.setAttribute("style", "color: #aaaaff");
     });
 }
 
-
-// app 
+// app
 
 const app = () => {
-    loadImages();
-}
+    // obtengo los divs con contenido
+    const divs = document.querySelectorAll("body > div:has(h2):has(img)");
+    // obtengo los elementos hijos
+    const h2s = Array.from(divs).map((div) => getFirstChildByType(div, "h2"));
+    const images = Array.from(divs).map((div) =>
+        getFirstChildByType(div, "img")
+    );
 
-document.addEventListener('DOMContentLoaded', app);
+    loadImages(images);
+};
+
+document.addEventListener("DOMContentLoaded", app);

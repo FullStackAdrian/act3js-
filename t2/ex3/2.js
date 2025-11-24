4cb3b4ca827ba5e8e240e418cb9610ec7744a0f6
adri <adrian.g.job@gmail.com>
2025-11-24 16:18:50 +0100

    modifyied without formdata

--- t2/ex3/2.js
+++ t2/ex3/2.js
@@ -2,23 +2,30 @@ const form = document.getElementById("form-validacio");
 
 // dado un form y unas keys te da un obj data.
 function getData(form, keys) {
-    const fd = new FormData(form);
-    const values = [...fd.values()];
-    const data = keys.reduce((obj, key, i) => {
-        obj[key] = values[i];
-        return obj;
-    }, {});
+    // const fd = new FormData(form);
+    // const values = [...fd.values()];
+    // const data = keys.reduce((obj, key, i) => {
+    //     obj[key] = values[i];
+    //     return obj;
+    // }, {});
+    
+    const nom = document.getElementById("nom2").value.trim();
+    const edat = Number(document.getElementById("edat2").value);
+    const email = document.getElementById("email2").value.trim();
+    const url = document.getElementById("url").value.trim();
+
+    const data = { nom: nom, edat: edat, email: email, url: url}
     return data;
 }
 
 function validate(data) {
 
     const errors = [];
-    console.log( data.nom);
-    if ( !data.nom || data.nom.length < 3 ) {
+    console.log(data.nom);
+    if (!data.nom || data.nom.length < 3) {
         errors.push("El nom és obligatori i ha de tenir mínim 3 caràcters.");
     }
-    if (! data.edat > 0) {
+    if (!data.edat > 0) {
         errors.push("L'edat ha de ser un número major que 0.");
     }
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
@@ -37,7 +44,7 @@ function validate(data) {
 }
 
 // las keys del formulario que queremos validar.
-const keys = [ "nom",
+const keys = ["nom",
     "edat",
     "email",
     "url"
